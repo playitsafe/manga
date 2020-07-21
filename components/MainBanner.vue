e3320f50c50a0202eacede450ee8eb8dbae82dbf
aaron <playitsafe@qq.com>
2020-07-21 14:28:08 +1000

    top navbar done

---
+++ components/MainBanner.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main-banner">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.main-banner {
+  height: 492px;
+  width: 100%;
+  background-image: url('');
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main">
+    indexx
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import '../assets/css/base.scss';
+
+.main {
+  height: 1000px;
+}
+
+</style>
