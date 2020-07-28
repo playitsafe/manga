8e2e44ee1ca31a9d119a4a7ffa4c512897a58658
aaron <playitsafe@qq.com>
2020-07-28 18:05:00 +1000

    toggle sidebar done

--- components/SideDrawer.vue
+++ components/SideDrawer.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="sidedrawer">
-    <span>最近观看</span>
-    <i class="fas fa-chevron-down"></i>
+  <div class="sidedrawer" :class="{hide: showSideBar}">
+    <div class="sidedrawer-btn" @click="showSideBar = !showSideBar">
+      <span>最近观看</span>
+      <i class="fas fa-chevron-down"></i>
+    </div>
+    <div class="sidedrawer-main">
+
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      showSideBar: false
+    }
+  }
 }
 </script>
 
@@ -15,21 +24,52 @@ export default {
 @import '@/assets/css/util.scss';
 
 .sidedrawer {
-  position: fixed;
-  padding: 0 15px;
-  font-size: 15px;
-  background: rgba(#fff, .65);
-  top: 55%;
-  right: 0;
-  height: 40px;
-  width: 156px;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-bottom-left-radius: 10px;
-  border-bottom-right-radius: 10px;
+  width: 115px;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  right: 0;
   z-index: 99;
-  transform: rotate(90deg);
-  transform-origin: top right;
+  background: rgba(#fff, .65);
+  overflow: show;
+  /* position: fixed;
+  right: 0;
+  display: flex; */
+  transform: translateX(0);
+  transition: transform ease .2s;
+
+  
+  &.hide {
+    transform: translateX(115px);
+  }
+  
+  &-btn {
+    background: rgba(#fff, .65);
+    position: absolute;
+    top: 55%;
+    right: 155px;
+    height: 40px;
+    width: 156px;
+    transform: rotate(90deg);
+    transform-origin: bottom right;
+
+    padding: 0 15px;
+    font-size: 15px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+    cursor: pointer;
+  }
+
+  &-main {
+    height: 100%;
+    width: 115px;
+
+  }
+
 }
+
 </style>
